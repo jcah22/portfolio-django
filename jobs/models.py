c23fd73a57d89175b37abb5286e87aa25d2bbcab
julscode <jcah549@gmail.com>
2024-04-10 15:53:19 -0600

    first commit

---
+++ jobs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Job(models.Model):
+    #images
+    image = models.ImageField(upload_to='images/')
+    #sumary
+    summary = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.summary
+    
+    
